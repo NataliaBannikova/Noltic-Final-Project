dc3e52f60a784042ef8dbcd75e42c834638cd369
Natalia Bannikova <natabannikova10@gmail.com>
2022-10-03 14:26:20 +0300

    updated lwc component

--- src/lwc/createVisit/createVisit.js
+++ src/lwc/createVisit/createVisit.js
@@ -1,25 +1,27 @@
 import {LightningElement, track, wire} from 'lwc';
 import { getObjectInfo} from 'lightning/uiObjectInfoApi';
-import getKids from '@salesforce/apex/VisitCardController.getKids';
+import getKids from '@salesforce/apex/CreateVisitController.getKids';
+import getResponsiblePersons from '@salesforce/apex/CreateVisitController.getResponsiblePersons';
 import Visit__c_OBJECT from '@salesforce/schema/Visit__c';
 import { createRecord } from 'lightning/uiRecordApi';
 import {ShowToastEvent} from "lightning/platformShowToastEvent";
-import {refreshApex} from "@salesforce/apex";
 
 export default class CreateVisit extends LightningElement {
     @wire(getObjectInfo, { objectApiName: Visit__c_OBJECT })
     objectInfo;
-    @wire(getKids)
-    kidsLst;
+    @wire(getResponsiblePersons)
+    responsiblePersons;
     @track value = '';
     @track optionsArray = [];
-    @track cardVisible = false;
-    timeFieldValue;
+    @track wasWithKidValue = '';
+    @track optionsWasWthKidArray = [];
+
+    // get Kids Records
 
     get options(){
         return this.optionsArray;
     }
-
+    @wire(getKids)
     connectedCallback() {
         getKids()
             .then(response=>{
@@ -30,28 +32,45 @@ export default class CreateVisit extends LightningElement {
                 this.optionsArray = arr;
             })
     }
+    // get Responsible Person Related to selected Kid
 
-    handleChangedKidValue(event){
+    get wasWithKidOptions(){
+        return this.optionsWasWthKidArray;
+    }
+
+    handleChangedKidValue(event) {
         this.value = event.target.value;
-        console.log(event.target.value);
+        getResponsiblePersons({selectedKidId: this.value})
+            .then(response => {
+                let arr = [];
+                for (let i = 0; i < response.length; i++) {
+                    arr.push({label: response[i].Name, value: response[i].Id})
+                }
+                this.optionsWasWthKidArray = arr;
+            })
+
     }
 
-    handleTimeChange(event){
-        this.timeFieldValue = event.detail.value;
+    handleChangedWhoWasValue(event){
+        this.wasWithKidValue = event.target.value;
     }
+    // create Visit
 
     createVisit(){
-        let fields = {'Kid__c' : this.value, 'Time_leaving__c' : this.timeFieldValue};
+        let fields = {'Kid__c' : this.value, 'Was_with_Kid__c' : this.wasWithKidValue};
         let objRecordInput = {'apiName' : 'Visit__c', fields};
         createRecord(objRecordInput).then(response => {
-            // alert('Visit created with Id: ' +response.id);
-                const evt = new ShowToastEvent({
-                    title: 'Visit Created '+ response.id,
-                    message: 'Operation successful ',
-                    variant: 'success',
-                    mode: 'dismissible'
-                });
-                this.dispatchEvent(evt);
+            const evt = new ShowToastEvent({
+                title: 'Visit Created '+ response.fields.Name.value,
+                message: 'Operation successful ',
+                variant: 'success',
+                mode: 'dismissible'
+            });
+            this.dispatchEvent(evt);
+            this.template.querySelectorAll('lightning-combobox').forEach(each => {
+                each.value = null;
+            });
+
 
         }).catch(error => {
             const evt = new ShowToastEvent({
